426d0b8deccbc8dd803a023664efb4b42eb598ff
stellastella <wuchukwustella333@gmail.com>
2016-11-30 18:07:22 +0100

    initial commit

--- app/library.js
+++ app/library.js
@@ -1,17 +1,19 @@
 module.exports = {
   findMissing: function (arr1, arr2) {
-  var diff;
-  var empty=[];
-    for (var i in arr2){
-    if (arr1.indexOf(arr2[i]) === -1) {
-     
-     diff= (arr2[i]);
-    } else if ((arr1 && arr2 )=== empty){
-      return 0;
-    } else if (arr1 === arr2){
-      return 0;
-    } 
-      }
-  return diff;
+var diff;
+for (var i in arr2){
+   if (arr1.indexOf(arr2[i]) === -1){
+    var diff = arr2[i];
+  }
+    else if(arr1.length === arr2.length) {
+       return 0;
+   }
+ 
 }
+if (arr1.length === 0 && arr2.length === 0){
+       return 0;
+   }
+return diff;
+}
+
 }
